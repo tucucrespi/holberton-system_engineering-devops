97b8ce9534bb517eb4ef668ac0bc7bfb8ae59d69
tucucrespi <tucucrespi@hotmail.com>
2021-04-06 01:32:02 +0000

    add task

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,4 @@
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => 'installed'
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,4 @@
+# Create a manifest that kills a process named killmenow
+exec { 'pkill':
+  command   => '/usr/bin/pkill -f killmenow'
+}
